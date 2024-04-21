3449f89e1411179eb9285be7ef86cfffd73d3bd7
anhassan <51217263+anhassan@users.noreply.github.com>
2024-04-21 01:55:32 -0400

    Update parallel_processing_framework_fn_args.py

--- parallel_processing_framework_fn_args.py
+++ parallel_processing_framework_fn_args.py
@@ -41,18 +41,20 @@ class ParallelExec:
         
         successful_jobs = []
         failed_args = []
+        results = []
         
         # Determining the jobs successful and failed jobs
         for index,job_state in enumerate(job_states):
             all_args = tuple(cnst_args+self.get_args_split(index,parallel_args))
             try:
-                job_state.result()
+                result = job_state.result()
                 successful_jobs.append(index)
+                results.append(result)
                 print(f"====>Job Passed with Split :{all_args}")
             except Exception as error:
                 print("E {}".format(error))
                 print(f"====>Job Failed with Split :{all_args}")
-                pass
+                results.append(None)
         
         # Getting the list of failed jobs for retrying
         failed_jobs_indices = [job_index for job_index in all_jobs_indices if job_index not in successful_jobs]
@@ -64,13 +66,23 @@ class ParallelExec:
         # Logging the list of failed jobs and failed job arguments for retrying
         print(f" Failed Job Numbers : {failed_jobs_indices}, Failed Jobs Parallel Splits : {failed_args}")
         
-        return failed_jobs_indices,failed_args
+        return failed_jobs_indices,failed_args,results
     
-
+    # Defining a utility function to combine splitted partial results
+    def combine_results(self,main_result,partial_result):
+        visited = []
+        for partial_index,partial_value in enumerate(partial_result):
+            for index,value in enumerate(main_result):
+                if value is None and index not in visited :
+                    main_result[index] = partial_value
+                    visited.append(index)
+                    break
+        return main_result
+    
     # Defining a robust way of running jobs in parallel and having retries on errors
     def run_parallel_with_retries(self,fn,*args,retries):
         # Getting the list of failed jobs and failed job arguments for retrying from the first run
-        failed_jobs_indices,failed_parallel_args = self.run_parallel(fn,*args)
+        failed_jobs_indices,failed_parallel_args,main_result = self.run_parallel(fn,*args)
         # Getting constant and parallel arguments from all the arguments
         cnst_args, parallel_args = self.get_cnst_parallel_args(*args)
         
@@ -81,11 +93,16 @@ class ParallelExec:
             print(f"All Splits being retried : {all_args}")
             
             # Running the failed jobs in parallel
-            failed_jobs_indices,failed_parallel_args = self.run_parallel(fn,*all_args)
+            failed_jobs_indices,failed_parallel_args,partial_result = self.run_parallel(fn,*all_args)
+            # Combining partial results with main results
+            main_result = self.combine_results(main_result,partial_result)
+            
             print("Retries Left - {}".format(retries))
             
             # Reducing the number of attempts left
             retries -=1
+            
+        return main_result
         
 
 # Defining the operational parameters
@@ -97,9 +114,12 @@ def foo(c1,c2,p1,p2):
     if (c1 == 'c1' and c2 == 'c2' and p1 == 3 and p2 == 9) or (c1 == 'c1' and c2 == 'c2' and p1 == 4 and p2 == 10) :
         raise Exception("FAILED....")
     else:
-        print(f"C1 : {c1} , C2 : {c2} , P1: {p1} , P2 : {p2}")
+        format_str = f"C1 : {c1} , C2 : {c2} , P1: {p1} , P2 : {p2}"
+        print(format_str)
+        return format_str
 
 # Using the parallel exection framework
 parallel_exec = ParallelExec(num_workers=max_workers)
-parallel_exec.run_parallel_with_retries(foo,"c1","c2",[1,2,3,4,5,6],[7,8,9,10,11,12],retries=3)
-    
+result = parallel_exec.run_parallel_with_retries(foo,"c1","c2",[1,2,3,4,5,6],[7,8,9,10,11,12],retries=3)
+
+print(result)
